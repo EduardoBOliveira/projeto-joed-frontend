b9c9354a6cd0414742951b559eeab31b9396871d
Eduardo <rm98890@fiap.com.br>
2024-04-17 16:53:13 -0300

    Configurando pagina Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,57 @@
-
+import Head from "next/head";
+import Icon from "@mdi/react";
+import { mdiCurrencyUsd } from "@mdi/js";
+import { mdiCartPlus } from "@mdi/js";
+import { mdiBookOpenVariant } from "@mdi/js";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-8xl mt-8">JOED</h1>
-      <h2 className="text mt-4">Monte seu negócio de forma simplificada</h2>
+      <Head>
+        <link rel="preconnect" href="https://fonts.google.com/" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <h1 className="text-8xl mt-8 font-lilita-one">JOED</h1>
+      <h2 className="text mt-4 font-lilita-one">
+        Monte seu negócio de forma simplificada
+      </h2>
 
       <div className="flex flex-wrap justify-between mt-32">
-        <fieldset className="bg-black text-white inline-block w-52 h-52 mx-24 mb-4 rounded-lg">
-          <p className="text-3xl">Produtos</p>
-        </fieldset>
-        <fieldset className="bg-black text-white inline-block w-52 h-52 mx-24 mb-4 rounded-lg">
-        <p>Rentabilidade</p>
-        </fieldset>
-        <fieldset className="bg-black text-white inline-block w-52 h-52 mx-24 mb-4 rounded-lg">
-        <p>Sobre</p>
-        </fieldset>
+        <a
+          href="/produtos"
+          className="w-52 h-52 mx-24 mb-4 rounded-lg inline-block"
+        >
+          <fieldset className="bg-black text-white w-full h-full flex flex-col justify-center items-center">
+            <p className="text-3xl font-krona-one">Produtos</p>
+            <Icon path={mdiCartPlus} size={5} />
+          </fieldset>
+        </a>
+        <a
+          href="/financas"
+          className="w-52 h-52 mx-24 mb-4 rounded-lg inline-block"
+        >
+          <fieldset className="bg-black text-white w-full h-full flex flex-col justify-center items-center">
+            <p className="text-3xl font-krona-one">Finanças</p>
+            <Icon path={mdiCurrencyUsd} size={5} />
+          </fieldset>
+        </a>
+        <a
+          href="/sobre"
+          className="w-52 h-52 mx-24 mb-4 rounded-lg inline-block"
+        >
+          <fieldset className="bg-black text-white w-full h-full flex flex-col justify-center items-center">
+            <p className="text-3xl font-krona-one">Sobre</p>
+            <Icon path={mdiBookOpenVariant} size={5} />
+          </fieldset>
+        </a>
       </div>
-
-      
-      
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        'lilita-one': ['Lilita One', 'sans-serif'],
+        'krona-one': ['Krona One', 'sans-serif'],
+      },
     },
   },
   plugins: [],
