7ada8ea728020a9a1e3034ae7f5144806961a88c
Eduardo <rm98890@fiap.com.br>
2024-04-20 17:14:04 -0300

    adicionando comentarios

--- src/app/loja/CadastrarProdutos/page.tsx
+++ src/app/loja/CadastrarProdutos/page.tsx
@@ -24,69 +24,72 @@ export default function ProdutosPage() {
       <NavBar active="loja"/>
       
       
-      <div className='flex justify-between gap-48'>
-        <Cardapio />
-        <div> {/* abertura da div do Titulo*/}
-          <h1>Add New Project</h1>
+      <div className='flex justify-betweens'>{/* abertura da div FULL-BODY*/}
+        
+        <div className='flex justify-between' > {/* abertura da div do Titulo*/}
+          <Cardapio />
+          <h1 className=''>Add New Project</h1>
         </div> {/* fechamento da div do Titulo*/}
 
-        <div> {/* abertura da div dos formularios NOME/PREÇO/DESCRIÇÃO*/}
-          <div> {/* abertura da div do formulario NOME*/}
+        <div className='flex justify-between gap-48 mt-40'>{/* abertura da div SEMI-BODY, contem elementos internos, deixando de fora /CARDAPIO e H1*/}
+          <div> {/* abertura da div dos formularios NOME/PREÇO/DESCRIÇÃO*/}
+            <div> {/* abertura da div do formulario NOME*/}
+              <FormControl component="form">
+                <TextField 
+                  label="Nome" 
+                  variant="outlined" 
+                  // value={nome} 
+                  // onChange={handleInputChange} -> onSubmit={handleSubmit}
+                  fullWidth 
+                  margin="normal" 
+                />
+                {/* <Button type="submit" variant="contained" color="primary">
+                  Enviar
+                </Button> */}
+              </FormControl>
+            </div>{/* fechamento da div do formulario NOME*/}
+            
+            <div>{/* abertura da div do formulario PREÇO*/}
             <FormControl component="form">
-              <TextField 
-                label="Nome" 
-                variant="outlined" 
-                // value={nome} 
-                // onChange={handleInputChange} -> onSubmit={handleSubmit}
-                fullWidth 
-                margin="normal" 
-              />
-              {/* <Button type="submit" variant="contained" color="primary">
-                Enviar
-              </Button> */}
-            </FormControl>
-          </div>{/* fechamento da div do formulario NOME*/}
-          
-          <div>{/* abertura da div do formulario PREÇO*/}
-          <FormControl component="form">
-              <TextField 
-                label="Preço" 
-                variant="outlined" 
-                // value={nome} 
-                // onChange={handleInputChange} -> onSubmit={handleSubmit}
-                fullWidth 
-                margin="normal" 
-              />
-              </FormControl>
-          </div>{/* fechamento da div do formulario PREÇO*/}
+                <TextField 
+                  label="Preço" 
+                  variant="outlined" 
+                  // value={nome} 
+                  // onChange={handleInputChange} -> onSubmit={handleSubmit}
+                  fullWidth 
+                  margin="normal" 
+                />
+                </FormControl>
+            </div>{/* fechamento da div do formulario PREÇO*/}
 
-          <div>{/* abertura da div do formulario DESCRIÇÃO*/}
-          <FormControl component="form">
-              <TextField 
-                label="Descrição" 
-                variant="outlined" 
-                // value={nome} 
-                // onChange={handleInputChange} -> onSubmit={handleSubmit}
-                fullWidth 
-                margin="normal" 
-              />
-              </FormControl>
-          </div>{/* fechmaneto da div do formulario DESCRIÇÃO*/}
+            <div>{/* abertura da div do formulario DESCRIÇÃO*/}
+            <FormControl component="form">
+                <TextField 
+                  label="Descrição" 
+                  variant="outlined" 
+                  // value={nome} 
+                  // onChange={handleInputChange} -> onSubmit={handleSubmit}
+                  fullWidth 
+                  margin="normal" 
+                />
+                </FormControl>
+            </div>{/* fechmaneto da div do formulario DESCRIÇÃO*/}
 
-        </div>{/* fechamento da div dos formularios NOME/PREÇO/DESCRIÇÃO*/}
+          </div>{/* fechamento da div dos formularios NOME/PREÇO/DESCRIÇÃO*/}
 
-        <div>{/* abertura da div dos campos Tamanho/Genero/Categoria*/}
-          jdsfijsdjfsdafasdkh
-        </div>{/* fechamento da div dos campos Tamanho/Genero/Categoria*/}
+          <div>{/* abertura da div dos campos Tamanho/Genero/Categoria*/}
+            jdsfijsdjfsdafasdkh
+          </div>{/* fechamento da div dos campos Tamanho/Genero/Categoria*/}
 
-        <div>{/* abertura da div do campo IMAGEM*/}
-          sjfsakojdojsad
-        </div>{/* fechamento da div do campo IMAGEM*/}
+          <div>{/* abertura da div do campo IMAGEM*/}
+            sjfsakojdojsad
+          </div>{/* fechamento da div do campo IMAGEM*/}
 
-        <div>{/* abertura da div do botão ADD PRODUCT*/}
-          soakdfpoaskdsa
-        </div>{/* abertura da div do botão ADD PRODUCT*/}
-      </div>
+          <div>{/* abertura da div do botão ADD PRODUCT*/}
+            soakdfpoaskdsa
+          </div>{/* abertura da div do botão ADD PRODUCT*/}
+        </div>{/* fechamento da div SEMI-BODY, contem elementos internos, deixando de fora /CARDAPIO e H1*/}
+      </div>{/* fechamento da div FULL-BODY*/}
       
       <Rodape/>
     </main>
