de36615f6e2ca1a5cc69ddfee871a5826a8d82e4
rm98890 <rm98890@fiap.com.br>
2024-04-23 22:32:38 -0300

    pagina cadastro de produtos com grid correto

--- src/app/loja/CadastrarProdutos/page.tsx
+++ src/app/loja/CadastrarProdutos/page.tsx
@@ -1,56 +1,90 @@
-// ProdutosPage.tsx
-"use client"
+'use client'
 
-import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { FormControl, TextField, Button, Input } from '@mui/material';
-
+import React from 'react';
+import { FormControl, TextField, Button, Input, InputLabel, Select, MenuItem } from '@mui/material';
+import FormularioCadastro from '@/components/FornularioCadastro';
+import Rodape from '@/components/Rodape';
 import NavBar from '@/components/NavBar';
 import Cardapio from '@/components/Cardapio';
-import Rodape from '@/components/Rodape';
-import FormularioCadastro from '@/components/FornularioCadastro';
 
 export default function ProdutosPage() {
+  const categorias = ['Categoria A', 'Categoria B', 'Categoria C']
+
   return (
     <main className="flex min-h-screen flex-col">
       <NavBar active="loja"/>
       
       <div className='flex justify-between'>
       
-      <Cardapio />
+      <Cardapio  />
       
-      <div className='flex flex-col text-center'>
-      <h1 className='mr-96'>Add New Project</h1>
+      <div className='flex flex-col text-center mt-4'>
+        <h1 className='mr-80 text-5xl'>Add New Project</h1>
 
-      <div className='flex justify-between mt-10'>
-        <div className='mr-96'>
-        <FormularioCadastro/>
-        <FormularioCadastro/>
-        <FormularioCadastro/>
-        </div>
+        <div className='flex justify-between mt-14'>
+          <div className='mr-80'>
+            <FormularioCadastro/>
+            <FormularioCadastro/>
+            <FormularioCadastro/>
+          </div>
 
-        <div className='mr-96'>
-        <FormularioCadastro/>
-        <FormularioCadastro/>
-        <FormularioCadastro/>
-        </div>
+          <div className='mr-80 flex flex-col gap-28'>
+            
+          <div>
+            <h2 className='text-2xl'>Tamanho</h2>
+              <Button variant="contained">P</Button>
+              <Button variant="contained">M</Button>
+              <Button variant="contained">G</Button>
+              <Button variant="contained">GG</Button>
+            </div>
+        
+            <div>
+            <h2>Gênero</h2>
+              <Button variant="contained">F</Button>
+              <Button variant="contained">M</Button>
+              <Button variant="contained">U</Button> {/* U para 'Unissex' */}
+            </div>
 
-        <div className='mr-96'>
-            <fieldset className='bg-white  w-52 h-52 mx-24 mb-4 '>
-              <Input type="file" accept="image/*" fullWidth margin="normal" className=' '/> 
+            <div className='flex flex-col gap-2'>
+              <div className='flex justify-between'>
+              <h2>Categoria</h2>
+              <Button variant="outlined" size="small">
+              Add Categoria
+              </Button>
+              </div> 
+              <FormControl fullWidth>
+                <InputLabel id="categoria-label">Selecione uma categoria</InputLabel>
+                <Select
+                  labelId="categoria-label"
+                  id="categoria-select"
+                  label="Selecione uma categoria"
+                >
+                {categorias.map((categoria, index) => (
+                  <MenuItem key={index} value={categoria}>
+                    {categoria}
+                  </MenuItem>
+                ))}
+                </Select>
+              </FormControl>
+              
+            </div>
+          </div>
 
+          <div className='mr-80 '>
+            <fieldset className='bg-white  w-60 h-52 mx-44 mb-4'>
+              <Input type="file" inputProps={{ accept: 'image/*' }} fullWidth sx={{ marginTop: 'normal' }} className=''/> 
             </fieldset>
-          
+          </div>
+        </div>
+        
+        <div className='flex justify-end mr-80'>
+          <Button type="submit" variant="contained" color="primary" className='flex w-10' >
+            Enviar
+          </Button>
         </div>
 
       </div>
-
-        <Button type="submit" variant="contained" color="primary" className=''>
-          Enviar
-        </Button>
-      </div>
         
-        
-
       </div> 
       
       <Rodape/>
--- src/components/Cardapio.tsx
+++ src/components/Cardapio.tsx
@@ -1,32 +1,32 @@
 import Link from "next/link";
 
-export default function Cardapio(){
-    return(
+export default function Cardapio() {
+  return (
     <div className="flex flex-1 items-start">
-        <fieldset className="bg-white text-black text-2xl flex flex-col h-full w-64">
-          <nav className="">
-            <ul className="flex flex-col gap-20 mt-20 mb-96 ml-2">
-              <li>
-                <Link href="/loja">Loja</Link>
-              </li>
-              <li>
-                <Link href="/loja/produtos">Produtos</Link>
-              </li>
-              <li>
-                <Link href="/loja/CadastrarProdutos">Cadastrar Produtos</Link>
-              </li>
-              <li>
-                <Link href="/loja/Vendas">Vendas</Link>
-              </li>
-              <li>
-                <Link href="/loja/analises">Analises</Link>
-              </li>
-              <li>
-                <Link href="/loja/promocoes">Promoções</Link>
-              </li>
-            </ul>
-          </nav>
-        </fieldset>
-      </div>
-      )
+      <fieldset className="bg-gradient-to-b from-purple-500 to-blue-500 text-white text-2xl flex flex-col h-full w-64 rounded-md">
+        <nav className="">
+          <ul className="flex flex-col gap-20 mt-20 mb-96 ml-2">
+            <li className="hover:bg-gray-300"> {/* Adicione a classe hover:text-gray-300 aqui */}
+              <Link href="/loja">Loja</Link>
+            </li>
+            <li className="hover:bg-gray-300"> {/* Adicione a classe hover:text-gray-300 aqui */}
+              <Link href="/loja/produtos">Produtos</Link>
+            </li>
+            <li className="hover:bg-gray-300"> {/* Adicione a classe hover:text-gray-300 aqui */}
+              <Link href="/loja/CadastrarProdutos">Cadastrar Produtos</Link>
+            </li>
+            <li className="hover:bg-gray-300"> {/* Adicione a classe hover:text-gray-300 aqui */}
+              <Link href="/loja/Vendas">Vendas</Link>
+            </li>
+            <li className="hover:bg-gray-300"> {/* Adicione a classe hover:text-gray-300 aqui */}
+              <Link href="/loja/analises">Analises</Link>
+            </li>
+            <li className="hover:bg-gray-300"> {/* Adicione a classe hover:text-gray-300 aqui */}
+              <Link href="/loja/promocoes">Promoções</Link>
+            </li>
+          </ul>
+        </nav>
+      </fieldset>
+    </div>
+  );
 }
