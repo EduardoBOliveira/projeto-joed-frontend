7fde0f32d9e2b72e1759c8376b7905ecc8e3f24c
rm98890 <rm98890@fiap.com.br>
2024-04-30 20:07:06 -0300

    continuando na pagina cadastro de categorias

--- src/app/loja/CadastroCategoria/page.tsx
+++ src/app/loja/CadastroCategoria/page.tsx
@@ -1,12 +1,47 @@
+'use client'
 import NavBar from "@/components/NavBar";
 import Cardapio from "@/components/Cardapio";
 import Rodape from "@/components/Rodape";
+import { FormControl, TextField } from "@mui/material";
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+
 
 export default function Home() {
-  return (
+    const [nome, setNome] = useState('');
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+      setNome(event.target.value);
+    };
+  
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      console.log('Nome inserido:', nome);
+      setNome('');
+    };
+  
+  
+    return (
     <main className="flex min-h-screen flex-col">
       <NavBar active="loja" />
+      
+      <div className="flex justify-between">
       <Cardapio />
+      <div>
+        <FormControl component="form" onSubmit={handleSubmit}>
+          <TextField 
+            label="Nome" 
+            variant="outlined" 
+            value={nome} 
+            onChange={handleInputChange} 
+            fullWidth 
+            margin="normal" 
+          />
+        </FormControl>
+      </div>
+    </div>
+      
+      
+      
       <Rodape/>
     </main>
   );
